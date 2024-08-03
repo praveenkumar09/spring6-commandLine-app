493121fefa1c204eafc1f31ed8f9b0c8829156ef
praveenkumar09 <tpkprav@gmail.com>
2024-08-03 19:10:38 +0800

    Implemented Student

--- src/main/java/com/praveen/springguru/spring6_commandLine_app/dao/StudentDAO.java
+++ src/main/java/com/praveen/springguru/spring6_commandLine_app/dao/StudentDAO.java
@@ -2,7 +2,13 @@ package com.praveen.springguru.spring6_commandLine_app.dao;
 
 import com.praveen.springguru.spring6_commandLine_app.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
     void save(Student student);
     Student findById(int id);
+    List<Student> findAll();
+    List<Student> findByLastName(String lastName);
+    List<Student> findByPredicateName(String firstName,String lastName);
+    List<Student> findByEmail(String email);
 }
--- src/main/java/com/praveen/springguru/spring6_commandLine_app/impl/StudentDAOImpl.java
+++ src/main/java/com/praveen/springguru/spring6_commandLine_app/impl/StudentDAOImpl.java
@@ -4,8 +4,11 @@ package com.praveen.springguru.spring6_commandLine_app.impl;
 import com.praveen.springguru.spring6_commandLine_app.dao.StudentDAO;
 import com.praveen.springguru.spring6_commandLine_app.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class StudentDAOImpl implements StudentDAO {
@@ -25,5 +28,33 @@ public class StudentDAOImpl implements StudentDAO {
     @Override
     public Student findById(int id) {
         return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        TypedQuery<Student> query = entityManager.createQuery("SELECT s FROM Student s", Student.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Student> findByLastName(String lastName) {
+        TypedQuery<Student> query = entityManager.createQuery("SELECT s FROM Student s WHERE s.lastName = :lastName", Student.class)
+                .setParameter("lastName", lastName);
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Student> findByPredicateName(String firstName, String lastName) {
+        TypedQuery<Student> query = entityManager.createQuery("SELECT s FROM Student s WHERE s.firstName = :firstName OR s.lastName = :lastName", Student.class)
+                .setParameter("firstName", firstName)
+                .setParameter("lastName", lastName);
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Student> findByEmail(String email) {
+        TypedQuery<Student> query = entityManager.createQuery("SELECT s FROM Student s WHERE s.email LIKE :email", Student.class)
+                .setParameter("email", "%" + email + "%");
+        return query.getResultList();
     }
 }
