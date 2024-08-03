cac4216bc99cf48da63ab750bc38bc48db672ba5
praveenkumar09 <tpkprav@gmail.com>
2024-08-03 10:41:49 +0800

    Initial Commit

---
+++ src/main/java/com/praveen/springguru/spring6_commandLine_app/Spring6CommandLineAppApplication.java
@@ -0,0 +1,13 @@
+package com.praveen.springguru.spring6_commandLine_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6CommandLineAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6CommandLineAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/praveen/springguru/spring6_commandLine_app/Spring6CommandLineAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praveen.springguru.spring6_commandLine_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6CommandLineAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
