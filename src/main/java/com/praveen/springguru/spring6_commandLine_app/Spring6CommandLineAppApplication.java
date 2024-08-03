3c51e92ed46710bf0c94662638aead3655d2f64d
praveenkumar09 <tpkprav@gmail.com>
2024-08-03 11:47:56 +0800

    Configuring Database & commind Line Runner Stuff

--- src/main/java/com/praveen/springguru/spring6_commandLine_app/Spring6CommandLineAppApplication.java
+++ src/main/java/com/praveen/springguru/spring6_commandLine_app/Spring6CommandLineAppApplication.java
@@ -1,13 +1,22 @@
 package com.praveen.springguru.spring6_commandLine_app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Spring6CommandLineAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Spring6CommandLineAppApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(){
+		return runner -> {
+			System.out.println("Hello World");
+		};
 	}
 
 }
